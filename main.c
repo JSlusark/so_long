ca5a5cefd0569f3a373ab7bf9f7c55fcd392c61e
Jessica Slusark <jslusark@c3a8c6.42berlin.de>
2024-10-09 11:32:31 +0200

    fixed premature freeing of map_data

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 13:06:39 by jjs               #+#    #+#             */
-/*   Updated: 2024/10/08 19:12:50 by jslusark         ###   ########.fr       */
+/*   Updated: 2024/10/09 11:22:26 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static	void launch_game(char **map_array, t_map *level)
 
 
 	game = mlx_init();
-	window = mlx_new_window(game, 64*10, 64*5, "Hello world!");
+	window = mlx_new_window(game, (64 * level->width), (64 * level->height), "Hello world!");
 	mlx_loop(game);
 	// // printf("character file path %d\n", map_data->character_img);
 	// game = mlx_init();
@@ -60,11 +60,11 @@ int	main(int argc, char **argv)
 	{
 		map_data = malloc(sizeof(t_map));
 		map_array = get_map(argv[1], map_data);
-		free(map_data);
 		collect_sprites(map_array, map_data);
 		// init_chara_data(map_array, &map_data);
 	printf("Location: main.c h:%d w:%d\n", map_data->height, map_data->width);
 		launch_game(map_array, map_data);
+		free(map_data);
 		free_map(map_array); // IMPORTANT!!
 	}
 	else
