57559069ced013989aa362d18cd1b66190512851
JSlusark <slusark.jessica@gmail.com>
2024-10-25 18:26:36 +0200

    renamed the sprite folder into texture as project's requirement

--- src/sprite_data/sprite_validation.c
+++ src/sprite_data/sprite_validation.c
@@ -6,7 +6,7 @@
 /*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 11:25:30 by jslusark          #+#    #+#             */
-/*   Updated: 2024/10/25 13:08:38 by jslusark         ###   ########.fr       */
+/*   Updated: 2024/10/25 18:22:06 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ static int	verify_sprites(void)
 	int	floor;
 	int	wall;
 
-	chara = open("sprites/xpm/chara.xpm", O_RDWR);
-	door = open("sprites/xpm/door.xpm", O_RDWR);
-	floor = open("sprites/xpm/floor.xpm", O_RDWR);
-	loot = open("sprites/xpm/loot.xpm", O_RDWR);
-	wall = open("sprites/xpm/wall.xpm", O_RDWR);
+	chara = open("textures/xpm/chara.xpm", O_RDWR);
+	door = open("textures/xpm/door.xpm", O_RDWR);
+	floor = open("textures/xpm/floor.xpm", O_RDWR);
+	loot = open("textures/xpm/loot.xpm", O_RDWR);
+	wall = open("textures/xpm/wall.xpm", O_RDWR);
 	if (chara < 0 || loot < 0 || door < 0 || floor < 0 || wall < 0)
 		return (0);
 	close(chara);
@@ -44,10 +44,10 @@ void	collect_sprites(char **map_array, t_map *level)
 		ft_printf("Error: unable to access one or more sprites\n");
 		exit(1);
 	}
-	level->character_img = "sprites/xpm/chara.xpm";
-	level->door_img = "sprites/xpm/door.xpm";
-	level->floor_img = "sprites/xpm/floor.xpm";
-	level->loot_img = "sprites/xpm/loot.xpm";
-	level->wall_img = "sprites/xpm/wall.xpm";
+	level->character_img = "textures/xpm/chara.xpm";
+	level->door_img = "textures/xpm/door.xpm";
+	level->floor_img = "textures/xpm/floor.xpm";
+	level->loot_img = "textures/xpm/loot.xpm";
+	level->wall_img = "textures/xpm/wall.xpm";
 	level->pixels = 64;
 }
